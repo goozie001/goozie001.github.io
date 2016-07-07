afd5f7cf8e2cdbc4eea0bf8d24dc6aae191bb8b7
Dylan Richardson <dylanrichardson47@gmail.com>
2016-07-06 19:40:04 -0700

    Update website

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,4 +1,19 @@
 var myApp = angular.module('myApp', []);
-myApp.controller('MainCtrl', ['$scope', function($scope) {
+myApp.controller('MainCtrl', ['$scope', '$document', function($scope, $document) {
 
+	$scope.activeId = -1;
+	
+	$scope.activate = function(chosen) {
+		$scope.activeId = chosen;
+	}
+	
+	$document.ready(function() {
+		$('.modal-trigger').leanModal({
+			complete: function() { 
+				$scope.activeId = -1;
+				$scope.$apply();
+			} // Callback for Modal close
+		}
+	  );
+	});
 }]);
